92105da785ddc2854e065dbc99abac5bf97d3233
Frank Abelbeck <frank.abelbeck@alumni.uni-hannover.de>
2019-09-21 22:36:11 +0200

    internal optimisations: stars are refreshed if out of view (now n_stars defines the number of concurrently visible stars); load buffer is pre-loaded and index-cycled (instead of using pop/insert); star refreshing is bundled into one separate loop

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ l_focal     = 40   # focal length; smaller: wider field of view; larger: narrowe
 z_max       = 1000 # starfield volume, z dimension: 0..z_max
 x_max       = 1000 # starfield volume, x dimension: -x_max..x_max
 y_max       = 1000 # starfield volume, y dimension: -y_max..y_max
-n_stars     = 50   # number of concurrently displayed stars inside the starfield volume
+n_stars     = 50   # number of concurrently displayed stars
 warp        = 4    # initial warp factor; can be increased/decreased with the bottom right/left buttons
                    #   the warp factor describes how fast the stars are moving
                    #   speed along z dimension: z = z - 2 * warp ** 2 (per frame)
@@ -33,15 +33,17 @@ shading     = 255  # colour of stars is white, scaled down to black with distanc
                    #   r = g = b = 255 - int(shading*z/z_max)
                    #   shading=0: no scaling
 		   #   shading=127: stars at z_max are grey
-		   #   shading=255: stars at z_max are black
+		   #   shading=255: stars at z_max are black (no pop-up)
 WIDTH       = 160  # screen width [px]
 HEIGHT      = 80   # screen height [px]
 buttonState = 0    # button state
 boolInfo    = True # show information on screen? (can be toggled with top right button)
-loadbuffer  = []   # used for timing (moving average filter)
 n_loadmavg  = 32   # number of cycle timing values (moving average filter)
+i_loadbuf   = 0    # current position in timing buffer
+loadbuffer  = [0]*n_loadmavg # timing buffer (moving average filter, pre-loaded buffer)
+toremove    = []   # list of star indices marked for removal
 if __name__ == "__main__":
-	# initialise starfield with stars at random positions
+	# initialise starfield with stars at random positions, sorted back to front
 	starfield = [[urandom.randint(-x_max,x_max),urandom.randint(-y_max,y_max),z,-1,-1] for z in sorted([urandom.randint(0,z_max) for i in range(0,n_stars)],reverse=True)]
 	# indefinite loop
 	while True:
@@ -66,40 +68,45 @@ if __name__ == "__main__":
 			if btns & buttons.BOTTOM_RIGHT == 0 and btns & buttons.BOTTOM_LEFT == 0:
 				# clear screen only if no warp buttons are held down
 				d.clear()
+			# replace obsolete stars
+			for i in toremove:
+				starfield[i] = [urandom.randint(-x_max,x_max),urandom.randint(-y_max,y_max),z_max,-1,-1]
+			# process all stars
+			toremove = []
 			for i,(x,y,z,sxold,syold) in enumerate(starfield):
-				# step 1: move star one step closer in z-direction
-				z = z - z_step
-				if z <= 0:
-					# z underflow: reset z, create new x/y coordinates
-					del starfield[i]
-					x = urandom.randint(-x_max,x_max)
-					y = urandom.randint(-y_max,y_max)
-					z = z_max
-					sxold = -1
-					syold = -1
-					starfield.insert(0,[x,y,z,sxold,syold])
-				else:
-					starfield[i][2] = z
-				# step 2: draw star at new position
+				# move star one step closer in z-direction
+				if z <= z_step:
+					# z underflow: mark star
+					toremove.append(i)
+				starfield[i][2] = z - z_step
+				# draw star at new position
 				sx = int(WIDTH/2 + l_focal*x/z)
 				sy = int(HEIGHT/2 + l_focal*y/z)
 				if sx >= 0 and sx < WIDTH and sy >= 0 and sy < HEIGHT:
+					# star in view: render it
 					c = 255 - int(shading*z/z_max)
 					if sxold >= 0 and syold >= 0:
+						# previous position available: draw line
 						d.line(sx,sy,sxold,syold,col=(c,c,c))
 					else:
+						# fresh star: draw pixel
 						d.pixel(sx,sy,col=(c,c,c))
-				starfield[i][3] = sx
-				starfield[i][4] = sy
+					starfield[i][3] = sx
+					starfield[i][4] = sy
+				else:
+					# star out of view: remove, create a new one
+					toremove.append(i)
 			# take cycle stop time and store in load buffer; remove oldest entries to keep a constant buffer size
-			loadbuffer.append(utime.time_ms()-t0)
-			while len(loadbuffer) > n_loadmavg: loadbuffer.pop(0)
+			dt = utime.time_ms() - t0
+			loadbuffer[i_loadbuf] = dt
+			i_loadbuf = (i_loadbuf + 1) % n_loadmavg
 			# finally, redraw, sleep and repeat; print additional info if activated
 			if boolInfo:
 				# calculate moving average of the load buffer and print information
-				cycleload = int(100 * (sum(loadbuffer) / len(loadbuffer)) / cycletime)
+				cycleload = int(100 * sum(loadbuffer) / n_loadmavg / cycletime)
 				d.print("warp {}".format(warp),fg=(192,192,192),posx=0,posy=0,font=0)
 				d.print("{}* load {}".format(n_stars,cycleload),fg=(192,192,192),posx=0,posy=70,font=0)
 			d.update()
-		# sleep only if there is time cycleload
-		utime.sleep_ms(cycletime-loadbuffer[-1])
+		# sleep only if there is time left in the cycle
+		if dt < cycletime:
+			utime.sleep_ms(cycletime-dt)
